f7798feced742f1fe714a987196ea62359712c72
showadock <lautaro.scholtus@gmail.com>
2024-09-23 20:15:46 -0300

    Añado cors para todas las rutas

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
 import messageController from "./controllers/messageController";
-
+import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 
 app.get("/ping", (req: Request, res: Response) => {
   return res.status(200).json({ message: "Pong" });
